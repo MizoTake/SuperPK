99091d478b48cf737aba43fc4f518804f8db235c
MizoTake <mizotake123@gmail.com>
2018-06-03 00:09:14 +0900

    開発準備

---
+++ Assets/PKVR/Scripts/ECS/Component/Stairs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace SuperPK
+{
+	public class Stairs : MonoBehaviour
+	{
+		public GameObject Supporter;
+		public int OneSideWidth;
+		public int StairsCount;
+	}
+}
