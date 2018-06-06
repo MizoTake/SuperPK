ac3038351b7134121276ea01e194cebcc61e3514
MizoTake <mizotake123@gmail.com>
2018-06-06 10:03:40 +0900

    add debug camera

---
+++ Assets/PKVR/Scripts/DebugController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (SystemInfo.supportsGyroscope)
+		{
+			Input.gyro.enabled = true;
+		}
+		transform.Translate (Vector3.up * 2.0f);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate (-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+	}
+}
